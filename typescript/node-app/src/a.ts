d3f2d5a46300250d237357307509580798f1433f
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-02-23 23:58:45 +0530

    interfaces

--- typescript/node-app/src/a.ts
+++ typescript/node-app/src/a.ts
@@ -46,34 +46,54 @@
 
 //--------------------------------------------------------
 
-interface User {
-    firstName:string,
-    lastName:string,
-    age: number,
-    email?:string
-};
+// interface User {
+//     firstName:string,
+//     lastName:string,
+//     age: number,
+//     email?:string
+// };
 
 
-function isLegal(user:User){
-    if(user.age > 18){
-        return true;
-    } else {
-        return false;
-    }
-}
+// function isLegal(user:User){
+//     if(user.age > 18){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-function greet(user:User){
-    console.log("hi there "+ user.firstName);
+// function greet(user:User){
+//     console.log("hi there "+ user.firstName);
     
-}
+// }
 
-const user = {
-    firstName: "sushil",
-    lastName: "kumar",
-    age: 20
-};
+// const user = {
+//     firstName: "sushil",
+//     lastName: "kumar",
+//     age: 20
+// };
 
-console.log(isLegal(user));
-greet(user);
+// console.log(isLegal(user));
+// greet(user);
 
+//---------------------------------------------------------
 
+ interface Person{
+    name:string;
+    age:number;
+    greet(phrase: string): void;
+ }
+
+ class Employee implements Person{
+    name:string;
+    age:number;
+
+    constructor(n:string, a:number){
+        this.name = n;
+        this.age = a;
+    }
+    greet(phrase:string){
+        console.log(`$(phrase) ${this.name}`);
+        
+    }
+ }
