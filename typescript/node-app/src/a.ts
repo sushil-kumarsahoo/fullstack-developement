ec7aa2068c51579228a75fd37cfc5fb8639c8f4d
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-02-24 23:24:22 +0530

    enum

--- typescript/node-app/src/a.ts
+++ typescript/node-app/src/a.ts
@@ -193,21 +193,20 @@
 
 //-------------------------------------------------------
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right
-}
+// enum Direction {
+//     Up ,
+//     Down ,
+//     Left ,
+//     Right 
+// }
 
-function doSomething(keyPressed: Direction){
-    if(keyPressed == Direction.Up){
+// function doSomething(keyPressed: Direction){
+//     if(keyPressed == Direction.Up){
 
-    }
-     
-}
+//     }    
+// }
 
-doSomething(Direction.Up);
-console.log(Direction.Down);
-console.log(Direction.Up);
+// doSomething(Direction.Up);
+// console.log(Direction.Down);
+// console.log(Direction.Up);
 
