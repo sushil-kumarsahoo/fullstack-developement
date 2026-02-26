7054ee9b3f49be8a9ac8652f9c20608ab0972b04
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-02-27 01:40:02 +0530

    advance query

---
+++ prisma/dist/advance-query-2.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({ log: ['info', 'query'] });
+//SELECT * FROM question OFFSET 0 LIMIT 10;
+//SELECT * FROM question OFFSET 10 LIMIT 10;
+//SELECT * FROM question OFFSET 20 LIMIT 10;
+async function main() {
+    let res = await prisma.user.findMany({
+        take: 3,
+        skip: 10
+    });
+    console.log(res);
+}
+main();
+prisma.$on("query", async (e) => {
+    console.log(`${e.query} ${e.params}`);
+});
+//# sourceMappingURL=advance-query-2.js.map
---
+++ prisma/dist/advance-query.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({ log: ['info', 'query'] });
+async function main() {
+    let res = await prisma.user.findMany({
+        where: {
+            email: {
+                endsWith: "gmail.com",
+            },
+            posts: {
+                some: {
+                    published: true,
+                },
+            },
+        },
+        include: {
+            posts: {
+                where: {
+                    published: true,
+                },
+            },
+        },
+    });
+    console.log(res);
+}
+main();
+//# sourceMappingURL=advance-query.js.map
