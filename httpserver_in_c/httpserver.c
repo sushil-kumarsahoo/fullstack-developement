fb0bd67f0508d1b3c9a27b8fafbfbdca34d257d3
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-02-28 15:46:44 +0530

    http server in c

--- httpserver_in_c/httpserver.c
+++ httpserver_in_c/httpserver.c
@@ -1,57 +1,59 @@
 #include <sys/socket.h>
 #include <stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <string.h>
 
-int main(void){
-    int rc = 0;
-    struct sockaddr_in bind_addr;
-   int ret = 0;
-   int tcp_socket = 0;
-   int client_socket = 0;
-    memset(&bind_addr, 0, sizeof(bind_addr));
+#define PORT 8080
+#define BUFFER_SIZE 4096
 
-     tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    char buffer[BUFFER_SIZE] = {0};
 
-    if(tcp_socket < 0){
-    perror("socket()");
-    return 1;
+    // Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { perror("socket()"); return 1; }
+    printf("Socket created ✅\n");
+
+    // Bind
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind()"); return 1;
+    }
+    printf("Bind succeeded ✅\n");
+
+    // Listen
+    if (listen(server_fd, 10) < 0) { perror("listen()"); return 1; }
+    printf("Listening on port %d...\n", PORT);
+
+    while (1) {
+        // Accept connection
+        client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd < 0) { perror("accept()"); continue; }
+        printf("Got connection!\n");
+
+        // Read request
+        memset(buffer, 0, BUFFER_SIZE);
+        read(client_fd, buffer, BUFFER_SIZE);
+        printf("Request:\n%s\n", buffer);
+
+        // Send response
+        char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 21\r\n"
+            "\r\n"
+            "Hello from C Server 🚀\n";
+        write(client_fd, response, strlen(response));
+
+        // Close client socket
+        close(client_fd);
+    }
+
+    close(server_fd);
+    return 0;
 }
- 
-
-printf("socket creation succeeded\n");
-
-   
-
-        bind_addr.sin_port = htons(8080);
-        bind_addr.sin_family = AF_INET;
-        bind_addr.sin_addr.s_addr = INADDR_ANY;
-
-    rc = bind(tcp_socket,(const struct sockaddr*)&bind_addr, sizeof(bind_addr));
-    if(rc <0 ){
-        perror("bind()");
-        return 1;
-    }
-    printf("bind succeeded\n");
-
-    
-
-
-    rc = listen(tcp_socket,SOMAXCONN );
-    if(rc < 0){
-        perror("listen()");
-        goto exit;
-    }
-    printf("listen succeeded\n");
-
-printf("waiting for connection...\n");
-     client_socket = accept(tcp_socket,NULL, NULL );
-
-    printf("got a connection!\n");
-
-    exit:
-    close(tcp_socket);
-    return ret;
-};
-
