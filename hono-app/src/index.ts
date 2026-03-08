7f79de6ed1d8e9efce416d81ef639520f4b38561
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-03-08 07:42:43 +0530

    middleware

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,7 +2,25 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+async function authMiddleware(c:any , next:any){
+  if(c.req.header("Authorization")){
+    const initTime = new Date();
+    await next()
+    const totalTime = (new Date().getTime() - initTime.getTime())/1000;
+    console.log(`it took ${totalTime} seconds`);
+    
+  } else {
+    return c.text("you dont have access");
+  }
+}
+
+
+app.post('/',authMiddleware, async(c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  
   
   return c.text('Hello Hono!')
 })
