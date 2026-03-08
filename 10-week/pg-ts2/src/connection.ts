2a195265d30574275697e06c8dd7f4f278ea35c3
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-03-08 08:49:08 +0530

    ...

--- 10-week/pg-ts2/src/connection.ts
+++ 10-week/pg-ts2/src/connection.ts
@@ -4,6 +4,6 @@ export const client = new Client({
   user: "postgres",
   host: "localhost",
   database: "postgres",
-  password: "sushil7847",
-  port: 5433, 
+  password: "password",
+  port: 5432, 
 });
