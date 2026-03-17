98372879aa9d421ed9cf35476a47df70fa7789a5
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-03-17 20:13:20 +0530

    created own package and publish to npm

---
+++ 12-week/create-own-packages/server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import authRouter from '././routes/auth.js';
+
+const app = express();
+
+app.use(express.json());
+
+// mount router
+app.use('/api/v1', authRouter);
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
