3f595ef9695c2acbc9bb64269c36a2b189574b03
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-03-26 14:32:36 +0530

    completed the middleware part

--- 13-week/medium-blog/backend/src/index.ts
+++ 13-week/medium-blog/backend/src/index.ts
@@ -12,6 +12,29 @@ type Bindings = {
 }
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.use('/api/v1/blog/*', async(c,next) => {
+  const auth = c.req.header('Authorization') || "";
+
+  if(!auth || !auth.startsWith('Bearer ')){
+     return c.json({ message: 'Forbidden'}, 403)
+  }
+
+  const token = auth.split(" ")[1]
+try{
+  const response = await verify(token, c.env.JWT_SECRET, 'HS256')
+  if(response.id){
+   await next()
+  }
+  else {
+    c.status(403)
+    return c.json({ error: "unauthorized"},403)
+  }
+}
+  catch (e) {
+    return c.json({ error: 'Invalid or expired token' }, 403)
+}
+})
+
 
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
