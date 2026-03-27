c2358289b5c49d3554e2e3a182a0e68348f1ed4a
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-03-27 08:56:45 +0530

    create a package and published to npm

---
+++ 13-week/medium-blog/common/src/index.ts
@@ -0,0 +1,33 @@
+import {z} from 'zod';
+
+export const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional()
+})
+
+export const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+})
+
+export const createBlogInput = z.object({
+    title:z.string(),
+    content:z.string()
+})
+
+export const updateBlogInput = z.object({
+    title:z.string(),
+    content:z.string(),
+    id:z.string()
+})
+
+
+export type signupInput = z.infer<typeof signupInput>
+
+export type signinInput = z.infer<typeof signinInput>
+
+export type createBlogInput = z.infer<typeof 
+createBlogInput>
+
+export type updateBlogInput = z.infer<typeof updateBlogInput>
