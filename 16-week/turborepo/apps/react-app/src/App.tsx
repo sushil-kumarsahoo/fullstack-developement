ececbd0e35e0c234df5fb9427cbe3fb702f2e150
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-09 23:13:45 +0530

    added a react froentend app and an express app

---
+++ 16-week/turborepo/apps/react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+
+function App() {
+  return (
+  <div>
+<Button appName="react">Hi there</Button>
+</div>
+ )
+}
+
+export default App
---
+++ 16-week/turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { Admin } from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/inputbox";
+
+export default function adminPage(){
+    return <div>
+        hi from admin page
+        <Admin/>
+        <InputBox/>
+    </div>
+}
