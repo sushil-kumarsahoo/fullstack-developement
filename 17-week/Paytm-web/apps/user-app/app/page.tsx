6f269a9ed6027f74de52a54b074659f69fb27786
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-17 22:25:04 +0530

    ...

--- 17-week/Paytm-web/apps/user-app/app/page.tsx
+++ 17-week/Paytm-web/apps/user-app/app/page.tsx
@@ -1,9 +1,12 @@
-import {prisma} from "@repo/db";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-export default function Page(){
-  return (
-    <div className="text-2xl">
-      hi there
-    </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
 }
