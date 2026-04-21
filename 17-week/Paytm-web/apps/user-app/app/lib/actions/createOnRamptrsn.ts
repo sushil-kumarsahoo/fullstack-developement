cee4ad4719c64f602cd3692e2b6d680a32f19fbc
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-21 11:41:29 +0530

    on ramp transaction

---
+++ 17-week/Paytm-web/apps/user-app/app/lib/actions/createOnRamptrsn.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import {prisma} from '@repo/db'
+import { revalidatePath } from "next/cache";
+
+
+export async function createOnRampTransaction(amount:number,provider:string){
+    const token = Math.random().toString();
+    const session = await getServerSession(authOptions);
+    const userId = session?.user.id;
+    if(!userId){
+        return {
+            message: "user not logged in"
+        }
+    }
+    prisma.onRampTransaction.create({
+        data:{
+           userId:Number(userId),
+           amount : amount * 100,
+           status:"Processing",
+           startTime: new Date(),
+           provider,
+           token:token,
+        }
+    });
+ 
+     revalidatePath("/transfer");
+
+    return {
+        message : "on ramp transaction added"
+    }
+
+}
