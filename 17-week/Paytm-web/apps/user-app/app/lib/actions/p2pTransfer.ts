bc591d4853a0888c5e9f78f994c8f797f877c165
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-22 20:54:48 +0530

    added locking to rows

--- 17-week/Paytm-web/apps/user-app/app/lib/actions/p2pTransfer.ts
+++ 17-week/Paytm-web/apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -25,6 +25,9 @@ export async function p2pTransfer(to: string, amount : number){
      }
 
      await prisma.$transaction(async(tx)=>{
+     
+        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+
         const fromBalance = await tx.balance.findUnique({
             where:{
                 userId:Number(from)
