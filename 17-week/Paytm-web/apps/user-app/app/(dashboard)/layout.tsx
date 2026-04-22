b9067db2fd14ddca2f64cebee518a52df491632a
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-04-22 19:58:50 +0530

    added another functionality p2p transfer

--- 17-week/Paytm-web/apps/user-app/app/(dashboard)/layout.tsx
+++ 17-week/Paytm-web/apps/user-app/app/(dashboard)/layout.tsx
@@ -14,6 +14,8 @@ export default function Layout({
                 <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+                <SidebarItem href={"/p2p"} icon={<P2PTransferIcon/>} title="P2P transfer" />
+
             </div>
         </div>
             {children}
@@ -31,6 +33,13 @@ function TransferIcon() {
     return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
   </svg>
+}
+
+function P2PTransferIcon(){
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
+</svg>
+
 }
 
 function TransactionsIcon() {
---
+++ 17-week/Paytm-web/apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,8 @@
+import { SendCard } from "../../../components/SendCard";
+
+
+export default function p2pPage(){
+    return <div className="w-full">
+       <SendCard/>
+    </div>
+}
